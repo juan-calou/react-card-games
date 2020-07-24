5aab034cd1228a5ecb38888f69114a5c7f9ba0be
Juan Calou <juan.calou@patagonian.it>
2020-07-24 11:59:12 -0300

    styles

--- src/pages/Guess.js
+++ src/pages/Guess.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+import CardDeck from "react-bootstrap/CardDeck";
+import Card from "react-bootstrap/Card";
 
 class Guess extends React.Component {
   constructor(props) {
@@ -7,66 +10,105 @@ class Guess extends React.Component {
     console.log("1. constructor()");
 
     this.state = {
-      deckid: 0,
-      card: "",
+      cards: [],
+      deck_id: 0,
+      remaining: 0,
+      loading: true,
     };
   }
 
-  async componentDidMount() {
-    console.log("3. componentDidMount()");
-    /// set state
+  fetchData = async () => {
     const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
     const response = await fetch(url);
     const data = await response.json();
     if (data.success) {
-      this.setState({ deckid: data.deck_id });
+      this.setState({
+        deck_id: data.deck_id,
+        remaining: data.remaining,
+        loading: false,
+      });
     }
-    console.log(this.state);
-  }
+    console.log(data);
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log("5. componentDidUpdate()");
-  }
-
-  componentWillUnmount() {
-    console.log("6. componentWillUnmount");
+  async componentDidMount() {
+    this.fetchData();
   }
 
   handleGetCard = async () => {
-    const id = this.state.deckid;
+    const id = this.state.deck_id;
     const url = `https://deckofcardsapi.com/api/deck/${id}/draw/?count=1`;
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ card: data.cards[0].image });
-    console.log(data.cards[0].image);
+    this.setState({
+      cards: data.cards,
+      deck_id: data.deck_id,
+      remaining: data.remaining,
+      loading: false,
+    });
+    console.log(data);
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <Button
-                variant="primary"
-                size="lg"
-                active
-                onClick={this.handleGetCard}
-              >
-                Get card
-              </Button>
-            </div>
-            {this.state.card !== "" ? (
+    if (this.state.loading) {
+      return <Spinner animation="border" variant="primary" />;
+    } else {
+      return (
+        <React.Fragment>
+          <div className="container">
+            <div className="row">
               <div className="col-12">
-                <img alt="card" src={this.state.card} />
+                <CardDeck>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </Card.Text>
+                      <Button
+                        variant="primary"
+                        size="lg"
+                        active
+                        onClick={this.handleGetCard}
+                      >
+                        Get card
+                      </Button>
+                    </Card.Body>
+                    <Card.Footer>
+                      <small className="text-muted">
+                        Deck ID {this.state.deck_id}
+                      </small>
+                    </Card.Footer>
+                  </Card>
+                  <Card>
+                    <Card.Body>
+                      {this.state.cards.length > 0 ? (
+                        <Card.Img
+                          bsPrefix="custom"
+                          src={this.state.cards[0].images.png}
+                        />
+                      ) : (
+                        <Card.Text>No card</Card.Text>
+                      )}
+                    </Card.Body>
+                  </Card>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>Card title</Card.Title>
+                      <Card.Text>
+                        Quedan {this.state.remaining} cartas en el mazo
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </CardDeck>
               </div>
-            ) : (
-              <div className="col-12">No card</div>
-            )}
+            </div>
           </div>
-        </div>
-      </React.Fragment>
-    );
+        </React.Fragment>
+      );
+    }
   }
 }
 
